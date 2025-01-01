24626a14b3c6d469405fd421b8b1f2bb41f25ebb
0skrrr <24061ok@creativehill.cz>
2025-01-01 18:07:40 +0100

    2025_01_01_

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const arrow = document.getElementById('arrow');
 const target = document.getElementById('seasonOne');
 
 arrow.addEventListener ('click', () => {
-    if (target.style.display === 'none') {
+    if (target.style.display === 'none' || target.style.display === '') {
         target.style.display = 'block';
     }
     
