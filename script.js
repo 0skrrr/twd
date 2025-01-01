1e99452cd89aed259a1db9d83d6e6125a6fcfc42
0skrrr <24061ok@creativehill.cz>
2025-01-01 19:15:17 +0100

    2025_01_01_

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 console.log("funguj");
-target.style.display === 'none';
+
 const arrow = document.getElementById('arrow');
 const target = document.getElementById('target');
+
+target.style.display === 'none';
 
 arrow.addEventListener('click', () => {
     if (target.style.display === 'none' || target.style.display === '') {
