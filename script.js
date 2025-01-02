877812beb47cf84d332965c3de7e526c81d617c8
0skrrr <24061ok@creativehill.cz>
2025-01-02 15:01:48 +0100

    2025_01_01_

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-console.log("Funguj");
-var arrow = document.getElementById("arrow");
-var target = document.getElementById("target");
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Funguj");
+    var arrow = document.getElementById("arrow");
+    var target = document.getElementById("target");
 
-target.classList.add("targetoff");
+    target.classList.add("targetoff");
 
-arrow.addEventListener("click", functionone);
+    arrow.addEventListener("click", functionone);
 
-function functionone () {
-    if (target.classList.contains("targetoff")) {
-        target.classList.add("targeton");
-        target.classList.remove("targetoff")
+    function functionone() {
+        if (target.classList.contains("targetoff")) {
+            target.classList.add("targeton");
+            target.classList.remove("targetoff");
+        } else {
+            target.classList.add("targetoff");
+            target.classList.remove("targeton");
+        }
     }
-    else {
-        target.classList.add("targetoff");
-        target.classList.remove("targeton");
-    }
-};
+});
